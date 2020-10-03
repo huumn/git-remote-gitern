8fa6ab5c83243b79a94f2d5fa24e213ec6931128
keyan <keyan.kousha+huumn@gmail.com>
2020-10-03 16:56:27 -0700

    some improvements very likely broken

--- crypt.js
+++ crypt.js
@@ -1,9 +1,4 @@
-#!/usr/bin/env node
 const { spawn } = require('child_process')
-const readline = require('readline')
-
-const object = process.argv[2]
-const spawnOpts = { shell: true, stdio: ['pipe', 'pipe', 'inherit'] }
 
 const en = async(input, output) => {
   input.write("hi\n", () => {
@@ -17,37 +12,7 @@ const de = async(input, output) => {
   rmHI.stdout.pipe(output)
 }
 
-// TODO: these should probably take an input and an output stream
-// might also only need one function
-const mutate = async (object) => {
-  const hashObject = spawn("git hash-object", ["-w", "--stdin"], spawnOpts)
-  const catFile = spawn("git cat-file", ["blob", object], spawnOpts)
-  hashObject.stdin.write("hi\n", () => {
-    catFile.stdout.pipe(hashObject.stdin)
-  })
-
-  const readHashObject = readline.createInterface({
-    input: hashObject.stdout,
-    terminal: false
-  })
-
-  for await (const resultObject of readHashObject) {
-    return resultObject
-  }
-}
-
-const unmutate = async (mutated) => {
-  const hashObject = spawn("git hash-object", ["-w", "--stdin"], spawnOpts)
-  const catFile = spawn("git cat-file", ["blob", mutated], spawnOpts)
-  catFile.stdout.pipe(rmHI.stdin)
-  rmHI.stdout.pipe(hashObject.stdin)
-
-  const readHashObject = readline.createInterface({
-    input: hashObject.stdout,
-    terminal: false
-  })
-
-  for await (const resultObject of readHashObject) {
-    return resultObject
-  }
+module.exports = {
+  en: en,
+  de: de,
 }
--- mirror.js
+++ mirror.js
@@ -1,42 +1,37 @@
 const { spawn, spawnSync } = require('child_process')
 const { exit } = require('process')
 const { resolve } = require('path')
-const readline = require('readline')
+const { lines } = require('./misc.js')
 const log = require('./logger.js')
 const crypt = require('./crypt.js')
 
-async function* lines(input) {
-  let rl= readline.createInterface({
-    input: input,
-    terminal: false
-  })
-  for await (const line of rl) {
-    yield line
-  }
-}
-
 class Mirror {
   constructor(src, dest, push) {
-    this.srcSpawnOpts = { shell: true, cwd: resolve(src), stdio: ['pipe', 'pipe', 'inherit'] }
-    this.destSpawnOpts = { shell: true, cwd: resolve(dest), stdio: ['pipe', 'pipe', 'inherit'] }
+    this.srcOpts = { cwd: resolve(src), stdio: ['pipe', 'pipe', 'inherit'] }
+    this.dstOpts = { cwd: resolve(dest), stdio: ['pipe', 'pipe', 'inherit'] }
     this.push = push
     this.transform = push ? crypt.en : crypt.de
-    log.debug("", this.srcSpawnOpts, this.destSpawnOpts)
+    log.debug("", this.srcOpts, this.dstOpts)
   }
-
   mirror = async (ref, remote) => {
-    let revList = spawn("git rev-list", ["--reverse", ref, "--not", `--remotes=${remote}`], this.srcSpawnOpts)
-  
+    // TODO: limit this to just the relevant objects so we don't have to recurse
+    // for non-new objects ... this will require some enref.map
+    let revList = spawn("git",
+                        ["rev-list", "--reverse", ref, "--not", `--remotes=${remote}`], 
+                        this.srcOpts)
+
     let parent = null
     for await (const commit of lines(revList.stdout)) {
       parent = await this.mirrorCommit(commit, parent)
     }
 
+    // TODO: should verify old ref - see manpage
     // update-ref <ref> <parent>
-    // TODO: will be worth verifying old ref, see manpage
     if (this.push && parent) {
       log.warn("git update-ref %s %s", ref, parent)
-      let updateRef = spawnSync("git update-ref", [ref, parent], this.destSpawnOpts)
+      let updateRef = spawnSync("git", 
+                                ["update-ref", ref, parent], 
+                                this.dstOpts)
       if (updateRef.status != 0) {
         log.error("failed to update-ref %s %s", ref, parent)
         exit(updateRef.status)
@@ -44,34 +39,50 @@ class Mirror {
     }
   }
 
-  mirrorCommit = async (commit, parent) => {
-    log.debug("mirroring commit %s with parent %s", commit, parent)
-  
-    // rewrite the tree
-    let tree = await this.mirrorTree(commit)
-  
-    // TODO: rewrite the commit with the old commit object as an encrypted message
-    // TODO: use commit-tree to rewrite the object
-    // NOTE: commit-tree gets author info from the command line and ..
-    //       we should override this info with something fun, e.g. leonardo da vinci
-    //       leonardo@mona.me
-    //     let commitTreeArgs = [tree]
-    // if (parent) {
-    //  commitTreeArgs.push("-p", parent)
-    // }
-    // commitTree = spawn("git commit-tree", commitTreeArgs, destSpawnOpts)
-    let commitTree = spawn("git hash-object", ["-w", "--stdin", "-t", "commit"], this.destSpawnOpts)
-    let catFile = spawn("git cat-file", ["commit", commit], this.srcSpawnOpts)
-    catFile.stdout.pipe(commitTree.stdin)
-    
-    let object = lines(commitTree.stdout)
-    log.debug("committed tree!", object)
-    return object
+  COMMIT_ENV = {
+    ...process.env, ...{
+      GIT_AUTHOR_DATE: "1977-06-10T12:00:00",
+      GIT_COMMITER_DATE="1994-10-13T12:00:00",
+      GIT_AUTHOR_EMAIL="leo@mona.me",
+      GIT_AUTHOR_NAME="Leonardo di ser Piero da Vinci",
+      GIT_COMMITTER_EMAIL="leo@mona.me",
+      GIT_COMMITTER_NAME="Leonardo di ser Piero da Vinci",
+    }
   }
 
-  mirrorObject = async(oid, type) => {
-    let hashObject = spawn("git hash-object", ["-w", "--stdin", "-t", type], this.destSpawnOpts)
-    let catFile = spawn("git cat-file", [type, oid], this.srcSpawnOpts)
+  mirrorCommit = async (commit, parent) => {
+    log.debug("mirroring commit %s with parent %s", commit, parent)
+    // rewrite the underlying tree
+    let tree = await this.mirrorTree(commit)
+
+    // TODO: we need to base64 encode/decode the message in addition to en/de
+    let res
+    if (this.push) {
+      // we encrypt the message and send to the encrypted message to commit-tree
+      let args = [tree]
+      if (parent) args.push("-p", parent)
+      let commitTree = spawn("git", args, {...this.dstOpts, env: this.COMMIT_ENV})
+      let catFile = spawn("git", 
+                          ["cat-file", "commit", commit], 
+                          this.srcOpts)
+      this.transform(catFile.stdout, commitTree.stdin)
+      res = lines(commitTree.stdout)
+    } else {
+      // decrypt the message of the commit and hash the message
+      //  as a commit object
+      let logMsg = spawn("git", ["log", "--format=%B", "-n", "1", commit], this.srcOpts)
+      let hashObject = spawn("git", ["hash-object","-w", "--stdin", "-t", "commit"], this.dstOpts)
+      this.transform(logMsg.stdout, hashObject.stdin)
+      res = lines(hashObject.stdout)
+    }
+
+    log.debug("mirrored commit!", res)
+    return res
+  }
+
+  mirrorObject = async (oid, type) => {
+    let hashObject = spawn("git", ["hash-object","-w", "--stdin", "-t", type], this.dstOpts)
+    let catFile = spawn("git", ["cat-file", type, oid], this.srcOpts)
     this.transform(catFile.stdout, hashObject.stdin)
     return lines(hashObject.stdout)
   }
@@ -81,17 +92,12 @@ class Mirror {
     // if object is a tree recurse effectively doing a depth first
     // rewrite of the object graph
     log.debug("mirroring tree %s", tree)
-  
-    let lsTree = spawn("git ls-tree", [tree], this.srcSpawnOpts)
-    // for await (const line of spawnLine(lsTree.stdout))
-    let readLsTree = readline.createInterface({
-      input: lsTree.stdout,
-      terminal: false
-    })
-  
-    // for await (const line of spawnLine(ls.Tree.stdout))
+
+    let lsTree = spawn("git ls-tree", [tree], this.srcOpts)
     let objs = ""
-    for await (const line of readLsTree) {
+
+    // TODO: encrypt file/dir names
+    for await (const line of lines(lsTree.stdout)) {
       log.debug("mirroring line %s", line)
       let [mode, type, oid, name] = line.split(/[ \t]/)
       switch (type) {
@@ -108,19 +114,15 @@ class Mirror {
           exit(1)
       }
     }
+
     log.debug("making tree with objects:\n%s", objs)
-    let mktree = spawn("git mktree", [], this.destSpawnOpts)
-    let readMkTree = readline.createInterface({
-      input: mktree.stdout,
-      terminal: false
-    })
+    let mktree = spawn("git", ["mktree"], this.dstOpts)
     mktree.stdin.write(objs)
     mktree.stdin.end()
-    for await (const object of readMkTree) {
-      log.debug("made tree %s", object)
-      return object
-    }
+    let object = lines(mktree)
+    log.debug("mirrored tree %s", object)
+    return object
   }
 }
 
-module.exports =  Mirror
+module.exports = Mirror
